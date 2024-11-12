418a5ff297e23c34136b2fbb9253e54b9fb83ca9
workahrbharani <workahrbharanikumar@gmail.com>
2024-11-12 18:01:57 +0530

    model string problem update

--- lib/models/service_list_model.dart
+++ lib/models/service_list_model.dart
@@ -4,62 +4,67 @@
 
 import 'dart:convert';
 
-ServiceListModel serviceListModelFromJson(String str) => ServiceListModel.fromJson(json.decode(str));
+ServiceListModel serviceListModelFromJson(String str) =>
+    ServiceListModel.fromJson(json.decode(str));
 
-String serviceListModelToJson(ServiceListModel data) => json.encode(data.toJson());
+String serviceListModelToJson(ServiceListModel data) =>
+    json.encode(data.toJson());
 
 class ServiceListModel {
-    String status;
-    List<ServicesListData> list;
-    String code;
-    String message;
+  String status;
+  List<ServicesListData> list;
+  String code;
+  String message;
 
-    ServiceListModel({
-        required this.status,
-        required this.list,
-        required this.code,
-        required this.message,
-    });
+  ServiceListModel({
+    required this.status,
+    required this.list,
+    required this.code,
+    required this.message,
+  });
 
-    factory ServiceListModel.fromJson(Map<String, dynamic> json) => ServiceListModel(
+  factory ServiceListModel.fromJson(Map<String, dynamic> json) =>
+      ServiceListModel(
         status: json["status"],
-        list: List<ServicesListData>.from(json["list"].map((x) => ServicesListData.fromJson(x))),
+        list: List<ServicesListData>.from(
+            json["list"].map((x) => ServicesListData.fromJson(x))),
         code: json["code"],
         message: json["message"],
-    );
+      );
 
-    Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() => {
         "status": status,
         "list": List<dynamic>.from(list.map((x) => x.toJson())),
         "code": code,
         "message": message,
-    };
+      };
 }
 
 class ServicesListData {
-    int id;
-    String name;
-    String description;
-    String price;
-    int status;
-    int createdBy;
-    DateTime createdDate;
-    int? updatedBy;
-    DateTime? updatedDate;
+  int id;
+  String? name;
+  String? description;
+  String? price;
+  int status;
+  int createdBy;
+  DateTime createdDate;
+  int? updatedBy;
+  DateTime? updatedDate;
 
-    ServicesListData({
-        required this.id,
-        required this.name,
-        required this.description,
-        required this.price,
-        required this.status,
-        required this.createdBy,
-        required this.createdDate,
-         this.updatedBy,
-         this.updatedDate,
-    });
+  ServicesListData({
+    required this.id,
+    this.name,
+    this.description,
+    this.price,
+    required this.status,
+    required this.createdBy,
+    required this.createdDate,
+    this.updatedBy,
+    this.updatedDate,
+  });
 
-    factory ServicesListData.fromJson(Map<String, dynamic> json) => ServicesListData(
+  factory ServicesListData.fromJson(Map<String, dynamic> json) =>
+      ServicesListData(
         id: json["id"],
         name: json["name"],
         description: json["description"],
@@ -68,10 +73,12 @@ class ServicesListData {
         createdBy: json["created_by"],
         createdDate: DateTime.parse(json["created_date"]),
         updatedBy: json["updated_by"],
-        updatedDate: json["updated_date"] == null ? null : DateTime.parse(json["updated_date"]),
-    );
+        updatedDate: json["updated_date"] == null
+            ? null
+            : DateTime.parse(json["updated_date"]),
+      );
 
-    Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() => {
         "id": id,
         "name": name,
         "description": description,
@@ -81,5 +88,5 @@ class ServicesListData {
         "created_date": createdDate.toIso8601String(),
         "updated_by": updatedBy,
         "updated_date": updatedDate?.toIso8601String(),
-    };
+      };
 }
